8d1abbdfbeaeb55411a75234c3f1878090de846f
jackvonhouse <caroxazone@gmail.com>
2023-09-15 13:46:32 +0300

    Update main function and golang dependencies

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,46 @@
 package main
 
-func main() {
+import (
+	"context"
+	"flag"
 
+	"github.com/jackvonhouse/auth-service/app"
+	"github.com/jackvonhouse/auth-service/config"
+	"github.com/jackvonhouse/auth-service/pkg/log"
+	"github.com/jackvonhouse/auth-service/pkg/shutdown"
+)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	logger := log.NewLogrusLogger()
+
+	var configPath string
+
+	flag.StringVar(
+		&configPath,
+		"config",
+		"config/config.toml",
+		"The path to the configuration file",
+	)
+
+	flag.Parse()
+
+	logger.Info("reading config...")
+
+	config, err := config.New(configPath, logger)
+	if err != nil {
+		logger.Error(err)
+
+		return
+	}
+
+	logger.Info("starting app...")
+
+	app := app.New(ctx, config, logger)
+
+	go app.Run()
+
+	shutdown.Graceful(ctx, cancel, logger, app)
 }
